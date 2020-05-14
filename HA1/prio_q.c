0dbe6807c5c7fb4eeee98f478004b4a4806e8e9c
AlexFares7 <65201278+AlexFares7@users.noreply.github.com>
2020-05-14 18:48:32 +0200

    Add files via upload

--- HA1/prio_q.c
+++ HA1/prio_q.c
@@ -1,10 +1,9 @@
 #include "prio_q.h"
 
-struct prio_q * prio_q_create() //creates a function that has a pointer to prio q as an output 
+struct prio_q * prio_q_create() 
 {
-	struct prio_q * q = malloc(sizeof(struct prio_q));
-    //defines a variable named q that can store an address of a variable of the type struct prio_q and allocates enough memory for the struct.
-	q->size = 0; //-> is used with pointers pointing to a structure to assigne values to its attributes.
+	struct prio_q * q = malloc(sizeof(struct prio_q)); //Pointer q to initialized priority queue.
+	q->size = 0; 
 	q->front = NULL;
 	printf("\n Created Queue");
 	return (q);
@@ -13,20 +12,20 @@ struct prio_q * prio_q_create() //creates a function that has a pointer to prio 
 
 void prio_q_enqueue(struct prio_q *q, void *data, int prio)
 {
-	struct prio_q_elem * elem = malloc(sizeof(struct prio_q_elem));
+	struct prio_q_elem * elem = malloc(sizeof(struct prio_q_elem)); //Pointer elem to initialized priority queue element
 	elem->data = data;
     elem->next = NULL;
 	elem->priority = prio;
 	
 
-	if (q->front == NULL || elem->priority > q->front->priority){
-		elem->next = q->front;
+	if (q->front == NULL || elem->priority > q->front->priority){ //Handels the cases of an empty priority queue or... 
+		elem->next = q->front;                                    //...when the newly added element has a higher priority than the front element.
 		q->front = elem;
 	}
 
-	else{
-		struct prio_q_elem * prev = q->front;
-    	while(prev->next != NULL && prev->next->priority > elem->priority){
+	else{ 
+		struct prio_q_elem * prev = q->front; 
+    	while(prev->next != NULL && prev->next->priority > elem->priority){ //slides the newly added element throught the queue until it reaches its correct position
         	prev=prev->next;
 		}
 		struct prio_q_elem * tmp = prev->next;
@@ -42,7 +41,7 @@ void * prio_q_dequeue(struct prio_q *q)
         return NULL;
 	}
 	else{
-		struct prio_q_elem * temp = q->front;
+		struct prio_q_elem * temp = q->front; //!!!
 		struct prio_q_elem * tmp = q->front->data;
 
 		q->front = q->front->next;
@@ -70,7 +69,7 @@ int prio_q_destroy(struct prio_q * q, void ** data)
 	int count = 0;
 
 	while (q->front != NULL){
-		struct prio_q_elem * tmp = q->front;
+		struct prio_q_elem * tmp = q->front;///!!!
 		if (q->front->data != NULL){
 			data[count] = q->front->data;
 			count++;
