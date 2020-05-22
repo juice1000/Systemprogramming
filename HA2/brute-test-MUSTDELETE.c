6a62f174f7f0cbd7901c974162984fb00aa16802
Julien Look <julien.look@campus.tu-berlin.de>
2020-05-22 19:02:58 +0200

    BRUTEF - update brute-test

--- HA2/brute-test-MUSTDELETE.c
+++ HA2/brute-test-MUSTDELETE.c
@@ -2,22 +2,38 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include "bruteforcer.h"
 
 int main(){
+
+    hashes *loaded_hashes = NULL;
     char filename[] = "";
+    //loaded_hashes->array = NULL;
     scanf("%s",filename);
-    FILE *fp = fopen(filename,"r");
-    int i = 0;
-    char HASH[256];
-    char strs[100][256];
-    while(fgets(HASH, sizeof(HASH),fp)!= NULL){
-        strcpy(strs[i],HASH);
-        //printf("%s\n",strs[i]);
-        i++;
-        
+    ////loaded_hashes = fopen(filename, "r");
+    //FILE *frt = fopen(filename, 'r');
+    //int i = 0;
+
+    loaded_hashes = load_hashes(filename);
+    //FILE *fp = fopen(filename,"r");
+    //int i = 0;
+    //char HASH[256];
+    //char strs[100][256];
+    //while(fgets(HASH, sizeof(load_hashes),fp)!= NULL){
+    //    strcpy(strs[i],HASH);
+    //    //loaded_hashes->array[i] -> strs[i];
+    //    
+    //    
+    //    //printf("%s\n",strs[i]);
+    //    i++;
+    //    
+    //}
+    //loaded_hashes->array = strs;
+    for (int i = 0; i < sizeof(loaded_hashes); i++){
+        printf("%s\n",loaded_hashes->array[i]);
     }
-    printf("%s",strs[6]);
     
-    fclose(fp);
+    
+    //fclose(fp);
     return 0;
 }
